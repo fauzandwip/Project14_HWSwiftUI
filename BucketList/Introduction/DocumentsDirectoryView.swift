f2d6cc7c7049eb691df43d080c95deb8486e9849
fauzandp <fauzan27dp@gmail.com>
2023-07-11 22:16:11 +0700

    Project14_HWSwiftUI | BucketList App Introduction Writing data to the documents directory

---
+++ BucketList/Introduction/DocumentsDirectoryView.swift
@@ -0,0 +1,41 @@
+//
+//  DocumentsDirectoryView.swift
+//  BucketList
+//
+//  Created by Fauzan Dwi Prasetyo on 11/07/23.
+//
+
+import SwiftUI
+
+struct DocumentsDirectoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        print(paths)
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+}
+
+struct DocumentsDirectoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DocumentsDirectoryView()
+    }
+}
